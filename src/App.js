fa36a1faccf57df5ad254319634eb91a35062fdf
= <=>
2022-12-05 11:04:32 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Footer from "./components/Footer";
+import LoadingBar from 'react-top-loading-bar'
+
+export default class App extends Component {
+  pageSize = 10;
+  apiKey = "c15fb2e1a8134baeaead6afe8fe79fd8"
+
+  state = {
+    progress: 0
+  }
+
+  setProgress = (progress) =>{
+    this.setState({
+      progress: progress,
+    })
+  }
+  render() {
+    return (
+      <>
+        <Router>
+          <Navbar />
+
+          <LoadingBar
+            color='#f11946'
+            height={3}
+            progress={this.state.progress}
+          />
+          <Routes>
+            <Route exact  path="/" element={<News setProgress={this.setProgress} apiKey={this.apiKey}   key="general"  pageSize={this.pageSize} country="in" category="general" heading="General" />}></Route>
+            <Route exact  path="/business" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="business"  pageSize={this.pageSize} country="in" category="business" heading="Business"/>}></Route>
+            <Route exact  path="/entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" heading="Entertainment" />}></Route>
+            <Route exact  path="/health" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="health" pageSize={this.pageSize}  country="in" category="health" heading="Health"/>}></Route>
+            <Route exact  path="/science" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="science" pageSize={this.pageSize} country="in" category="science" heading="Science"/>}></Route>
+            <Route exact  path="/sports" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="sports" pageSize={this.pageSize} country="in" category="sports" heading="Sports"/>}></Route>
+            <Route exact  path="/technology" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="technology" pageSize={this.pageSize} country="in" category="technology" heading="Technology"/>}></Route>
+          </Routes>
+        </Router>
+        <Footer/>
+      </>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div className='footer bg-dark p-4 mt-4'>
+        <p className='text-center text-light'>@2022-2023, www.INDNews.com, publishedAt: NOV 2022</p>
+      </div>
+    )
+  }
+}
+
+export default Footer
